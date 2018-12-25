f1a64fd72d83d21b362e7f64a6305afb0fecce16
tearyRain <1165127476@qq.com>
2018-12-25 22:38:45 +0800

    modify loginService

--- src/main/java/com/teddy/service/LoginService.java
+++ src/main/java/com/teddy/service/LoginService.java
@@ -1,9 +1,24 @@
 package com.teddy.service;
 
+import com.teddy.dao.AdminDao;
+import com.teddy.dao.SponsorDao;
+import com.teddy.dao.StudentDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class LoginService {
+
+    @Autowired
+    AdminDao adminDao;
+
+    @Autowired
+    StudentDao studentDao;
+
+    @Autowired
+    SponsorDao sponsorDao;
 
     boolean checkAdminPassword(Long id, String password) {
         // TODO
