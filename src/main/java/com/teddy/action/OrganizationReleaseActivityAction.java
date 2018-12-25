f9d157b9842fb042eebfeab0024caa4bd218e32b
WangZhiwenCN <1763362044@qq.com>
2018-12-25 23:34:06 +0800

    some action

---
+++ src/main/java/com/teddy/action/OrganizationReleaseActivityAction.java
@@ -0,0 +1,31 @@
+package com.teddy.action;
+
+import com.teddy.entity.Activity;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.struts2.convention.annotation.*;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+
+@Controller
+@Scope("prototype")
+@ParentPackage("struts-default")
+@Namespace(value = "/")
+@Results({
+        @Result(name = "success", location = "/welcome.jsp")
+})
+
+public class OrganizationReleaseActivityAction {
+    @Getter
+    @Setter
+    Activity activity;
+
+    @Action(value = "organizationReleaseActivity")
+
+    public String execute(){
+        return SUCCESS;
+    }
+
+}
