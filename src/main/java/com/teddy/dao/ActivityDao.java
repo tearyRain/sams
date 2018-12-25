4413d98087c3fd297acba935051c5d145d49229d
YuGan <792850842@qq.com>
2018-12-25 22:33:35 +0800

    fix daos

--- src/main/java/com/teddy/dao/ActivityDao.java
+++ src/main/java/com/teddy/dao/ActivityDao.java
@@ -1,13 +1,40 @@
 package com.teddy.dao;
 
+import com.teddy.entity.Activity;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ActivityDao {
     @Autowired
     private SessionFactory sessionFactory;
 
+    public void findById(Long id) {
+        sessionFactory.getCurrentSession().find(Activity.class, id);
+    }
+
+    public List<Activity> findByName(String name) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Activity> query = session.createQuery("from Activity as s where s.name = ?1", Activity.class);
+        query.setParameter(1, name);
+        return query.list();
+    }
+
+    public void save(Activity activity) {
+        sessionFactory.getCurrentSession().save(activity);
+    }
+
+    public void saveOrUpdate(Activity activity) {
+        sessionFactory.getCurrentSession().saveOrUpdate(activity);
+    }
+
+    public void update(Activity activity) {
+        sessionFactory.getCurrentSession().update(activity);
+    }
 
 }
--- src/main/java/com/teddy/dao/SponsorDao.java
+++ src/main/java/com/teddy/dao/SponsorDao.java
@@ -1,12 +1,39 @@
 package com.teddy.dao;
 
+import com.teddy.entity.Sponsor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class SponsorDao {
     @Autowired
     private SessionFactory sessionFactory;
 
+    public void findById(Long id) {
+        sessionFactory.getCurrentSession().find(Sponsor.class, id);
+    }
+
+    public List<Sponsor> findByName(String name) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Sponsor> query = session.createQuery("from Sponsor as s where s.name = ?1", Sponsor.class);
+        query.setParameter(1, name);
+        return query.list();
+    }
+
+    public void save(Sponsor sponsor) {
+        sessionFactory.getCurrentSession().save(sponsor);
+    }
+
+    public void saveOrUpdate(Sponsor sponsor) {
+        sessionFactory.getCurrentSession().saveOrUpdate(sponsor);
+    }
+
+    public void update(Sponsor sponsor) {
+        sessionFactory.getCurrentSession().update(sponsor);
+    }
 }
