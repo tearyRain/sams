bc2e67a3b2d7f9ce224b2de71abd366ab52be300
JiashengHong <984041815@qq.com>
2019-01-01 01:13:53 +0800

    modify studentDao

--- src/main/java/com/teddy/dao/AttendanceDao.java
+++ src/main/java/com/teddy/dao/AttendanceDao.java
@@ -21,17 +21,6 @@ public class AttendanceDao {
         return sessionFactory.getCurrentSession().find(Attendance.class, id);
     }
 
-    public Attendance findByStudentAndActivity(Long studentId, Long activityId){
-        Session session = sessionFactory.getCurrentSession();
-        Query<Attendance> query = session.createQuery("from Attendance where student.id = ?1 and activity.id = ?2");
-        query.setParameter(1, studentId).setParameter(2, activityId);
-        List<Attendance> list = query.list();
-        if (list == null || list.isEmpty()) {
-            return null;
-        }
-        return list.get(0);
-    }
-
     public void save(Attendance attendance) {
         sessionFactory.getCurrentSession().save(attendance);
     }
